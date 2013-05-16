b294bbc9c393fe28dad5285e761c4312d0baab08
S. van Laere <webkluns@gmail.com>
2013-05-16 16:20:16 +0200

    Create nl-message.php

---
+++ i18n/nl-message.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Wolf CMS link-tracker plugin language file
+ *
+ * @package Translations
+ */
+
+return array(
+    'You have modified this page.  If you navigate away from this page without first saving your data, the changes will be lost.' => 'You have modified this page.  If you navigate away from this page without first saving your data, the changes will be lost.',
+  
+	//----------------
+	'Add' => 'Toevoegen',
+	'Edit' => 'Bewerken',
+	'Tracker' => 'Tracker',
+	'Settings' => 'Instellingen',
+	'Click here' => 'Klik hier',
+	'Add tracker' => 'Tracker toevoegen',
+	'Link Tracker' => 'Tracker Link',
+	'Documentation' => 'Documentatie',
+	'Manage trackers' => 'Trackers beheren',
+	'No records exist.' => 'Er bestaan geen records',
+	'to create a record.' => 'een record aanmaken.',
+	'Unable to create database table.' => 'Niet in staat om database-tabel te maken.',
+	'Track links throughout your WolfCMS website.' => 'Track koppelingen in uw WolfCMS website.',
+	
+	/* Data table columns */
+	'Name' => 'Naam',
+	'URL' => 'URL',
+	'Action' => 'Actie',
+	'# Clicks' => '# Klikken',
+	
+	/* Errors */
+	'Record not found.' => 'Record niet gevonden.',
+	'Please enter a URL.' => 'Geef een URL op.',
+	'No records selected.' => 'Geen records geselecteerd.',
+	'Post data not found.' => 'Bericht data niet gevonden.',
+	'Please enter a name.' => 'Geef een naam op',
+	'Invalid action selected.' => 'Ongeldige actie geselecteerd.',
+	'Unable to create record.' => 'Niet in staat om record te maken.',
+	'Unable to update record.' => 'Niet in staat om record bij te werken.',
+	'A record with this name already exists.' => 'Een record met deze naam bestaat reeds.',
+	'No data to export.' => 'Geen gegevens te exporteren.',
+	
+	/* Success messages */
+	':n records deleted.' => ':n records verwijderd.',
+	':n records updated.' => ':n records bijgewerkt.',
+	'Record has been created.' => 'Record is aangemaakt.',
+	'Record has been updated.' => 'Record is bijgewerkt.',
+	
+	/* Index form */
+	'Update' => 'Bijwerken',
+	'Delete' => 'Verwijderen',
+	'No action' => 'Geen actie',
+	'Reset stats' => 'Statistieken resetten',
+);
